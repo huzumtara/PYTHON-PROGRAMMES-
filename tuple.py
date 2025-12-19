3d58979252c8a3bb02e70380171d8978cad99cac
Huzum <huzumtara@gmail.com>
2025-12-19 22:28:09 +0530

    add tuple .py

---
+++ tuple.py
@@ -0,0 +1,19 @@
+ 
+ #Example tuple
+ numbers= (1, 2, 3, 2, 4, 2, 5)
+# 1. count() → Count occurrences of a value
+count_of_2 = numbers.count(2)
+print("Number of times 2 appears:", count_of_2)
+
+# 2. index() → Find the position of the first occurrence
+pos_of_3 = numbers.index(3)
+print("Index of 3:", pos_of_3)
+
+
+
+
+
+
+
+
+
