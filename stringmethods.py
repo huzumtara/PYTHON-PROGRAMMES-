c58778ceeecb0c68ed1643554b7ab92ddf2e20e5
Huzum <huzumtara@gmail.com>
2025-12-21 21:35:02 +0530

    add add stringmethods.py

---
+++ stringmethods.py
@@ -0,0 +1,36 @@
+#string mrthods.py
+"HELLO".lower()
+"hello".upper()
+
+"python".capitalize()
+
+"hello world".title()
+
+" hello ".strip()
+
+" hello".lstrip()
+
+"hello ".rstrip()
+
+"apple".replace("a","A")
+
+"A,B,C".split()
+
+"-".join(["a,b"])
+
+"apple".find("p")
+
+"apple".count("p")
+
+"apple".index("p")
+
+"python".startswith("py")
+
+"python".endswith("on")
+
+"123".isdigit()
+
+"abc".isalpha()
+
+"12ab".isalnum()
+
