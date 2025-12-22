3cf7c4fd10644c96489ff292418561e956010fba
Huzum <huzumtara@gmail.com>
2025-12-22 10:49:55 +0530

    dictionary.py

---
+++ dictionary keys.py
@@ -0,0 +1,5 @@
+# Program: Exploring dictionary keys, values, and items
+ student = {"name": "Ravi", "age": 21, "course": "Python"} 
+print("Keys:", student.keys())
+ print("Values:", student.values())
+ print("Items:", student.items())
