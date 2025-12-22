76615390488517ebc22869392732e17d1ab0bb09
Huzum <huzumtara@gmail.com>
2025-12-22 22:07:17 +0530

    add dictupdate.py

---
+++ dictupdate.py
@@ -0,0 +1,7 @@
+#updatekey in dict
+student={"Huzum":40,"kiran":50,"likitha":90}
+print("keys:",student.keys())
+print("values:",student.values())
+print("items:",student.items())
+student.update({"raj": 80})
+print(student)
