4e4a2f03dea46e49d12a7cc6fe40b51a1a4acda8
Huzum <huzumtara@gmail.com>
2025-12-28 12:17:43 +0530

    add condition.py

---
+++ Documents/PYTHON-PROGRAMMES/condition.py
@@ -0,0 +1,10 @@
+ #conditional statements
+
+def even_odd(num):
+    if num % 2 == 0:
+        print(f"{num} is Even")
+    else:
+        print(f"{num} is Odd")
+#test calls
+even_odd(7)
+even_odd(2)
