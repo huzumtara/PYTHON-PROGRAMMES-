7089ae340c902a36e88591b9dde7b77dd3c62cfd
Huzum <huzumtara@gmail.com>
2025-12-29 22:44:38 +0530

    huzum.py

---
+++ huzum.py
@@ -0,0 +1,9 @@
+# printing the program using the while condition 
+n=int(input("enter the number"))
+i=1
+
+while i<=10:
+    print(f"{n} x{i} = {n*i}")
+    i=i+1
+else:
+    print(f"table completed")
