3ae5a9ff7961e56b82b8c0efb57bf938610f874c
Huzum <huzumtara@gmail.com>
2025-12-30 11:21:22 +0530

    data.py

---
+++ data.py
@@ -0,0 +1,7 @@
+#checking multiple data type
+data=[1,3,4,[6,7,9],0,6]
+for item in data:
+    if isinstance (item,list):
+        print(f"this is a list",item)
+    else:
+        print(f"this is a number",item)    
