60972156d7738259e746a1c46c908911c0e715aa
Huzum <huzumtara@gmail.com>
2026-01-04 22:01:07 +0530

    add keyarg.py

---
+++ keyarg.py
@@ -0,0 +1,4 @@
+#key arguments in functions
+def key_arg(name,age,city):
+    print(f"Hai myself{name} and my age is {age} ,im living inthe city {city}")
+key_arg(name="Huzum",age=28,city="hyd")    
