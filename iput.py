3a44aff0e843e1d29e96c1401d16664b55895c9a
Huzum <huzumtara@gmail.com>
2026-01-05 15:48:42 +0530

    add iput.py

---
+++ iput.py
@@ -0,0 +1,10 @@
+#using input function
+def add(english_marks,hindi_marks):
+    return (english_marks+hindi_marks)
+english_marks=int(input("enter the english_marks:"))
+                  
+hindi_marks=int(input("enter the hindi_marks:"))
+   
+print(add(english_marks,hindi_marks))                
+ 
+
