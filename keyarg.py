5dae0be88f54862d3462c7c2bb1c001d26c7de47
Huzum <huzumtara@gmail.com>
2026-01-05 11:22:20 +0530

    add keyarg.py

---
+++ keyarg.py
@@ -0,0 +1,6 @@
+# arbitrary arguments(keyword arguments)
+def add_4(**kwargs):
+    print("keyword arguments:",kwargs)
+    print(type(kwargs))
+    
+print(add_4(name="HUZUM",age=28,city="hyderabad",salary=1000000))
