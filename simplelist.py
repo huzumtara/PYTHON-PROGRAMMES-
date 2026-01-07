f31652b4182f3f55661a5c088825731c135b9c06
Huzum <huzumtara@gmail.com>
2026-01-07 22:27:20 +0530

    add simplelist.py

---
+++ simplelist.py
@@ -0,0 +1,17 @@
+# simple list 
+my_list=[1,3,5,6,7]
+print(my_list[4])
+print(my_list[3])
+print(my_list[1])
+#modifying my list
+print(my_list[1])==100
+print(my_list)
+#adding the value to the list 
+my_list.append(80)
+print(my_list)
+#inserting the value in the list 
+my_list.insert(2,40)
+print(my_list)
+#removing the value in the list 
+my_list.remove(6)
+print(my_list)
